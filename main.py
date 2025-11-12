36558376d71afaf829ef5b336b1ddd06a8d6d669
PUNIT PATEL <punitpatel@PUNITs-MacBook-Pro.local>
2025-11-12 12:33:40 +0530

    added CORS

--- main.py
+++ main.py
@@ -59,6 +59,7 @@ app = FastAPI(title="AI Text Detector API", version="2.0", lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+    "https://ai-detector-app.vercel.app",
     "http://localhost:5174",
     "http://localhost:5173",
     "http://127.0.0.1:3000",],
